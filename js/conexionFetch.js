f17ba1918b59399123dc80727ffc2b220c2923fe
Leonardo Alvarez <brayanleonardo721@gmail.com>
2023-09-28 18:37:39 -0500

    feat - agrego puesto

--- js/conexionFetch.js
+++ js/conexionFetch.js
@@ -137,7 +137,7 @@ curriculum = `
                 <div class="informacion__contenido">
                     <h4 class="no-margin  subtitulos">Puesto solicitado</h4>
                     <p><span class="informacion__bold">Puesto: </span><span>{puesto}</span></p>
-                    <p><span class="informacion__bold">Puesto: </span><span>Programar senior</span></p>
+                    <p><span class="informacion__bold">Puesto: </span><span>Programardor senior</span></p>
 
                 </div>
             </article>
